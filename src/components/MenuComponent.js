c40a5558d8d2f960846236cbdaaf9426f54903a3
Code Sport I/O <CodeSportCares@codesport.io>
2020-06-15 17:27:30 -0400

    Complete Assignment 3: Moved Dish Detail to Separate Companent DishDetailsComponent.js

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, /*CardText, CardBody,*/ CardTitle } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends React.Component {
 
@@ -11,15 +12,16 @@ class Menu extends React.Component {
     }
 
 
-    onDishSelect(dish){//how to change of the component
+    onDishSelect(dish){//how to change of the state of a class component
         this.setState({ selectedDish: dish})
 
     }
 
+
     renderDish(dish){
         if(dish != null){
-
-            return(//'top' may be subsitituted for 
+/*
+            return(//'top' may be subsitituted for width="100%"
                 <Card> 
                     <CardImg width="100%" src={dish.image} alt={dish.name} /> 
                     <CardBody>
@@ -28,21 +30,27 @@ class Menu extends React.Component {
                     </CardBody>
                 </Card>
             )
+*/
 
+            return(
+            
+                <DishDetail selectedDish={this.state.selectedDish} />
+                
+                )
         }else{
 
             return(<div></div>)
         }
 
-
-
     }
+
+
 
     render() {
         const menu = this.props.dishes.map((dish) =>{ //note the use of this.props.dishes.map( and not this.state.dishes.map(
             return(//key={dish.id} can be in either the <Card> or the <div>, not both
-                <div className="col-12 col-md-5 m-1"> 
-                    <Card key={dish.id}
+                <div key={dish.id} className="col-12 col-md-5 m-1"> 
+                    <Card 
                     onClick={() => this.onDishSelect(dish)}>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
@@ -58,12 +66,9 @@ class Menu extends React.Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
+                {this.renderDish(this.state.selectedDish)} 
             </div>
+
         );
     }
 
