c8e6540d5ac88e64fdcaea094ebcfab6cdb68772
Code Sport I/O <CodeSportCares@codesport.io>
2020-06-15 17:31:29 -0400

    Complete Assignment 3: Moved Dish Detail to Separate Companent DishDetailComponent.js

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Card, CardImg, /*CardImgOverlay,*/ CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends React.Component{
+/*
+//Useless constructor. No state being handled here. State management lifted up and handled by MenuComponent.js
+//Can this class component can be converted to a function instead?
+    constructor (props){
+        super(props);
+    }
+*/
+
+    renderDish(dish){
+        return(
+            <Card> 
+                <CardImg width="100%" src={dish.image} alt={dish.name} /> 
+                <CardBody>
+                    <CardTitle><h4>{dish.name}</h4></CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>   
+        )
+    }
+
+    renderComments(comments){
+        return(    
+                
+                <Card> 
+                <CardBody>
+                    <CardTitle><h4>Comments</h4></CardTitle>
+                    <ul className="list-unstyled">
+                        {comments.map( (singleComment) =>
+                            <li key={singleComment.id}>{singleComment.comment}<br />
+                            -- {singleComment.author}, {singleComment.date}<br /><br />
+                            </li>
+                        )}
+                    </ul>
+                </CardBody>
+                </Card>
+        )
+    }
+
+    render(){
+
+        return(
+
+            <div className="row">
+            
+                <div className="col-12 col-md-5 m-1"> 
+                    {this.renderDish(this.props.selectedDish) /*selectedDish =name of prop based from MenuComponent.js*/}           
+                </div>
+
+                <div className="col-12 col-md-5 m-1">  
+                    {this.renderComments(this.props.selectedDish.comments)}
+                </div>
+
+            </div>
+
+        );
+
+
+    }
+
+
+
+
+}
+
+
+export default DishDetail;
